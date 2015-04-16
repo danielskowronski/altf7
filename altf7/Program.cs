cb7057e75ed1ac7f5c0cdce96cd3c2bbdf924f03
Daniel Skowroński <daniel@dsinf.net>
2015-04-16 09:00:57 +0200

    keyboard hook integrated
    global keyboard hook for alt+f7 works
    program flow as desired

--- altf7/Program.cs
+++ altf7/Program.cs
@@ -11,12 +11,10 @@ namespace altf7
 {
     class Program
     {
-        #region dllimport
+        #region dllimport + const + hook
 
         [DllImport("user32.dll")]
         private static extern IntPtr GetForegroundWindow();
-        [DllImport("user32.dll")]
-        private static extern IntPtr GetParent(IntPtr hWnd);
         [DllImport("user32.dll")]
         private static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
         [DllImport("user32.dll", EntryPoint = "SetWindowPos")]
@@ -29,21 +27,38 @@ namespace altf7
         public static extern short GetAsyncKeyState(UInt16 virtualKeyCode);
 
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        private static extern IntPtr SetWindowsHookEx(int idHook,
-            LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
 
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
         private static extern bool UnhookWindowsHookEx(IntPtr hhk);
 
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode,
-            IntPtr wParam, IntPtr lParam);
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
 
         [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         private static extern IntPtr GetModuleHandle(string lpModuleName);
 
-        #endregion dllimport
+        private static IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc,
+                    GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private delegate IntPtr LowLevelKeyboardProc(
+            int nCode, IntPtr wParam, IntPtr lParam);
+
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private const int WM_SYSKEYDOWN = 0x0104;
+        private static LowLevelKeyboardProc _proc = HookCallback;
+        private static IntPtr _hookID = IntPtr.Zero;
+
+        #endregion dllimport + const + hook
 
         #region operating functions
         static void setForegroundWindowPos(int x, int y)
@@ -76,6 +91,21 @@ namespace altf7
         {
             return GetAsyncKeyState(1) != 0 || GetAsyncKeyState(2) != 0;
         }
+
+        static bool lastWasAlt = false;
+        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && (wParam == (IntPtr)WM_KEYDOWN) || (wParam == (IntPtr)WM_SYSKEYDOWN))
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+                Console.WriteLine((Keys)vkCode + "\t" + vkCode.ToString());
+                if (vkCode == 164) lastWasAlt = true;
+                else if (vkCode == 118 && lastWasAlt) moveForegroundWindowUntilClick();
+                else lastWasAlt = false;
+            }
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
         #endregion operating functions
 
         #region manual testing functions
@@ -102,40 +132,7 @@ namespace altf7
             }
         }
         #endregion manual testing functions
-
-
-        #region kbd
-        // http://blogs.msdn.com/b/toub/archive/2006/05/03/589423.aspx
-        private const int WH_KEYBOARD_LL = 13;
-        private const int WM_KEYDOWN = 0x0100;
-        private static LowLevelKeyboardProc _proc = HookCallback;
-        private static IntPtr _hookID = IntPtr.Zero;
-
-        private static IntPtr SetHook(LowLevelKeyboardProc proc)
-        {
-            using (Process curProcess = Process.GetCurrentProcess())
-            using (ProcessModule curModule = curProcess.MainModule)
-            {
-                return SetWindowsHookEx(WH_KEYBOARD_LL, proc,
-                    GetModuleHandle(curModule.ModuleName), 0);
-            }
-        }
-
-        private delegate IntPtr LowLevelKeyboardProc(
-            int nCode, IntPtr wParam, IntPtr lParam);
-
-        private static IntPtr HookCallback(
-            int nCode, IntPtr wParam, IntPtr lParam)
-        {
-            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
-            {
-                int vkCode = Marshal.ReadInt32(lParam);
-                Console.WriteLine((Keys)vkCode);
-            }
-            return CallNextHookEx(_hookID, nCode, wParam, lParam);
-        }
-        #endregion kbd
-        static void doMagic()
+        static void moveForegroundWindowUntilClick()
         {
             while (true)
             {
@@ -143,17 +140,16 @@ namespace altf7
                 point p = getMousePos();
                 setForegroundWindowPos(p.x, p.y);
                 Console.WriteLine(p.x + "\t" + p.y + "\t" + title);
+                if (mouseClicked()) break;
             }
         }
 
         static void Main(string[] args)
         {
-            //doMagic();
-            //getMousePosLoop();
-            
             _hookID = SetHook(_proc);
-            getMousePosLoop();
+            Application.Run();
             UnhookWindowsHookEx(_hookID);
         }
+
     }
 }
